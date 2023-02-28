4231b25749727d6b0249f868ef06d8d11df7e198
TuDQ <quoctu2014@gmail.com>
2023-02-28 23:43:22 +0700

    Updates

---
+++ _next/static/n-URP7AsMz66pc7fWd-be/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e0ddc15058303980.css","static/chunks/pages/index-ca833d0ea2e73a31.js"],"/_error":["static/chunks/pages/_error-91a854d9c9cfa29b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
