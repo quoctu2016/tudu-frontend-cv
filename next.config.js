0de5f5a4da1881b6f62c0d9ce3bd1b1156242f2f
TuDQ <quoctu2014@gmail.com>
2023-03-01 00:01:45 +0700

    Create page about

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
+  // experimental: {
+  //   appDir: true,
+  // },
+  images: {
+    unoptimized: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
